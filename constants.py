units corrected

constants.py
@@ -3,10 +3,14 @@ from scipy.constants import physical_constants
 
 amu = physical_constants["atomic mass constant"]
 k = physical_constants["Boltzmann constant"]
+na = physical_constants["Avogadro constant"]
 
 periodic_table = {
         "Ar": (18, 39.95) 
         }
 
-lj_interaction_table = {("Ar", "Ar"): (0.185, 3.5)}
+#lj_interaction_table = {("Ar", "Ar"): (0.2381, 3.405e-10)}
+lj_interaction_table = {("Ar", "Ar"): (1.65e-21, 3.4e-10)}
+#lj_interaction_table = {("Ar", "Ar"): (0.185, 3.5)}
+#lj_interaction_table = {("Ar", "Ar"): (0.294, 3.73)} # FAKE ARGON
 

energy.py
@@ -14,7 +14,7 @@ def lennard_jones(atom1, atoms, dists):
     ratio = np.divide(sigma, norm)
     energy = np.sum(4*epsilon*(ratio**12 - ratio**6))
     d_energy = (24*epsilon/norm)*(2*ratio**12 - ratio**6)
-    gradient = np.sum(d_energy.reshape(-1,1)*dists, axis=0)
+    gradient = np.sum(d_energy.reshape(-1,1)*(dists/norm.reshape(-1,1)), axis=0)
     return energy, gradient
 
 def calculate_energy_grad(atoms, coords, box):
@@ -22,7 +22,7 @@ def calculate_energy_grad(atoms, coords, box):
     for n_coord, coord in enumerate(coords):
         coords_tmp = mdtools.pbc_ortho(coords.copy(), box, coord)
         kdtree = spatial.KDTree(coords_tmp)
-        nbs = kdtree.query_ball_point(np.asarray([0,0,0]), 9)
+        nbs = kdtree.query_ball_point(np.asarray([0,0,0]), 9e-10)
         nbs.remove(n_coord)
         energy, grad = lennard_jones(atoms[n_coord], atoms[nbs], coords_tmp[nbs])
         energies.append(energy)

iotools.py
@@ -7,11 +7,11 @@ def readxyz(filename):
     atoms = xyzfile.iloc[:,0].to_numpy()
     coords = xyzfile.iloc[:,1:4].to_numpy()
     #return atoms, coords
-    return np.full(10, 'Ar'), (np.random.rand(10, 3) - np.array([.5, .5, .5]))*2*10
+    return np.full(3, 'Ar'), (np.random.rand(3, 3) - np.array([.5, .5, .5]))*2*10*1e-10
 
 def writexyz(atoms, coords):
     at_df = pd.DataFrame(atoms)
-    coord_df = pd.DataFrame(coords)
+    coord_df = pd.DataFrame(coords*1e10)
     merged = pd.concat([at_df, coord_df], axis=1)
     with open("traj.xyz", 'a+') as outfile:
         outfile.write(F"{len(atoms)}\n\n")

main.py
@@ -2,7 +2,7 @@ import iotools
 import mdtools
 import energy
 import numpy as np
-from constants import amu, periodic_table
+from constants import amu, na, periodic_table
 import configparser
 import thermostat
 
@@ -11,7 +11,7 @@ config = configparser.ConfigParser()
 config.read(settings)
 xyzfile = config["system"]["xyzfile"]
 ensemble = config["system"]["ensemble"]
-box = tuple(float(i) for i in config["system"]["box"].strip('[]').split())
+box = tuple(float(i)*1e-10 for i in config["system"]["box"].strip('[]').split())
 Ti = float(config["system"]["starting temperature"])
 Tf = float(config["system"]["temperature"])
 dt = float(config["simulation"]["dt"])
@@ -29,18 +29,21 @@ v = mdtools.generate_initial_velocities(atoms, m, Ti)
 _, grad = energy.calculate_energy_grad(atoms, coords, box)
 a = - grad / m.reshape(-1,1)
 while it < max_it:
-    if it%10000 == 0:
+    if it%1000 == 0:
         print(it)
     coords += v*dt + 0.5*a*(dt**2)
     mdtools.pbc_ortho(coords, box)
-    _, a_new = energy.calculate_energy_grad(atoms, coords, box)
-    print(np.sum(_))
-    a_new /= -m.reshape(-1,1)
+    _, grad = energy.calculate_energy_grad(atoms, coords, box)
+    if it%100 ==0:
+        print((np.sum(_)/4184)*na[0])
+    a_new = - grad / m.reshape(-1,1)
     v += 0.5*(a_new + a)*dt
     if ensemble == "NVT":
         T = ts.measure(v, m)
+#        if it%1000 == 0:
+#            print(T)
         v = ts.rescale(v, T)
     a = a_new
-    if it%1 ==0:
+    if it%100 ==0:
         iotools.writexyz(atoms, coords)
     it += 1

mdtools.py
@@ -10,7 +10,7 @@ def generate_initial_velocities(atoms, m, T):
         transl_vec = np.full(3, maxwell(scale=a).stats('m')/2)
         distr = maxwell.rvs(scale=a, size=3)
         v.append((distr - transl_vec)*2)
-    return np.array(v)*1e10
+    return np.array(v)
 
 def pbc_ortho(coords_copy, box, point=np.array([0,0,0])):
     coords_copy -= point

thermostat.py
@@ -19,7 +19,7 @@ class Scaling(Thermostat):
     def __init__(self, Tf):
         super().__init__(Tf)
 
-    def rescale(self, v, T, Tf):
+    def rescale(self, v, T):
         self.c = np.sqrt(self.Tf/T)
         v *= self.c
         return v
@@ -31,8 +31,8 @@ class Berendsen(Thermostat):
         self.dt = dt
         self.tau = tau
 
-    def rescale(self, v, T, Tf):
-        self.c = np.sqrt(1 - (self.dt/self.tau)*(1-self.T_f/T))
-        v += self.c
+    def rescale(self, v, T):
+        self.c = np.sqrt(1 - (self.dt/self.tau)*(1-self.Tf/T))
+        v *= self.c
         return v
     